f57cb6788cdc1675f34b0c04bec537e02e1ba72a
Ved-sketch <vedgupta1806@gmail.com>
2025-07-28 01:49:38 +0530

    added the styling to the first card, completed it from the perspective of mobile view.

--- src/Card1.jsx
+++ src/Card1.jsx
@@ -3,10 +3,10 @@ import narutoImage from './assets/naruto image.jpeg'
 export default function Card1(){
     return(
         <>
-            <h1>Skill Card</h1>
+            <h1 className="card-title">Skill Card</h1>
             <img src={narutoImage} alt="profile-image" className="profile-image"/>
             <div id="info">
-                <h1>Ved Gupta</h1>
+                <h1 id="name">Ved Gupta</h1>
                 <h3>Aspiring Web Dev</h3>
                 <h3 id="college-name">Indian Institute of Information Technology, Vadodara</h3>
                 <h4 id="email">vedgupta1806@gmail.com</h4>
