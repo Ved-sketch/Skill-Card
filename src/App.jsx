d98ffa7a78a8431255ed49f343ebe41e1c76449b
Ved-sketch <vedgupta1806@gmail.com>
2025-07-28 20:35:42 +0530

    added the Card2 skeleton as well as added the toggle button to navigate between card1 and card2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 
 import './App.css'
 import Card1 from './Card1.jsx'
+import Card2 from './Card2.jsx'
+import { useState } from "react"
 
 function App() {
-  return(
-      <Card1 className="card"/>
-  );
+    const [showCard1, setShowCard1] = useState(true);
+
+    function handleToggleClick(){
+      setShowCard1(!showCard1);
+    }
+    return(
+
+      <div id="app-container"> {/* A container for the cards*/}
+        {showCard1 ? (<Card1 className="card" />) : (<Card2 className="card" />)}
+
+        <button className="toggle-button" onClick={handleToggleClick}>
+          {showCard1 ? "Show Backside" : "Show FrontSide"}
+        </button>
+      </div>
+    );
 }
 
 export default App
---
+++ src/Card2.jsx
@@ -0,0 +1,25 @@
+export default function Card2(){
+    return (
+        <>
+            <div className="web-tech">
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>React</li>
+                </ul>
+            </div>
+
+            <div className="languages">
+                <ul>
+                    <li>Python</li>
+                    <li>C++</li>
+                    <li>C</li>
+                </ul>
+            </div>
+
+            <a id="github" href="https://github.com/Ved-sketch" >GitHub</a>
+            <a id="leetcode" href="https://leetcode.com/u/Ved_Gupta06/">Leetcode</a>
+        </>
+    );
+}
